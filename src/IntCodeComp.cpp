18cef82cc49381997355d2719cccac8f30e37ed5
gmsfyi <git@gms.fyi>
2020-01-17 20:34:14 +0100

    test2

--- src/IntCodeComp.cpp
+++ src/IntCodeComp.cpp
@@ -6,7 +6,6 @@ IntCodeComp::IntCodeComp()
     std::cout << "IntCodeComp initialized!" << std::endl;
 }
 
-
 IntCodeComp::~IntCodeComp()
 {
     std::cout << "IntCodeComp deconstructed!" << std::endl;
